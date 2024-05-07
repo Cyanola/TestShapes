d86584d0c45ed91daa3cf8f215bd2a3d992fe9de
Ольга Артемова <artemovao771@gmail.com>
2024-05-07 19:49:32 +0300

    create readme

--- src/main/java/com/example/testshapes/Test/TestHighlightCircle.java
+++ src/main/java/com/example/testshapes/Test/TestHighlightCircle.java
@@ -8,36 +8,6 @@ import java.util.List;
 import java.util.Random;
 
 public class TestHighlightCircle {
-//    private Shape figure;
-//    private Color originalFill;
-//    private Color highlightedFill;
-//    private Color originalBorderFill;
-//    private Color highlightedBorderFill;
-//
-//    public TestHighlightCircle(Shape figure) {
-//
-////        this.figure = figure;
-////        this.originalFill = figure.getColorFill();
-////        this.highlightedFill = Color.YELLOW;
-////        this.originalBorderFill = figure.getColorBorder();
-////        this.highlightedBorderFill = Color.YELLOW;
-//
-////        canvas.setOnMouseEntered(enteredHandler);
-////        canvas.setOnMouseExited(exitedHandler);
-//    }
-//
-//    private void enteredHandler() {
-//        figure.setColorFill(highlightedFill);
-//        figure.setColorBorder(highlightedBorderFill);
-//    }
-//
-//    ;
-//
-//    // Метод для установки цвета выделения
-//    public void setHighlightedFill(Color color1, Color color2) {
-//        this.highlightedFill = color1;
-//        this.highlightedBorderFill = color2;
-//    }
 Random rnd =new Random();
     public static void main(String[] args) {
         double x=200, y=200;
@@ -57,20 +27,10 @@ Random rnd =new Random();
         // Тесты для закрашивания объекта
         testCases.add(new Circle(9, Color.CORAL, Color.BLACK, 50, 90));
         testSelecteds.add(new Circle(9, Color.YELLOW, Color.YELLOW, x, y));
-
         testCases.add(new Circle(9, Color.CORAL, Color.BLACK, 98, 90));
-//        testSelecteds.add(new Circle(9, Color.YELLOW, Color.YELLOW, x, y));
-
         testCases.add(new Circle(9, Color.MISTYROSE, Color.BLACK, 59, 91));
-//        testSelecteds.add(new Circle(9, Color.YELLOW, Color.YELLOW, x, y));
-
         testCases.add(new Circle(9, Color.SIENNA, Color.BISQUE, 99, 81));
-//        testSelecteds.add(new Circle(9, Color.YELLOW, Color.YELLOW, x, y));
-
         testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 350, 390));
-
-
-
         testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 99, 390));
         testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 250, 99));
         testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 100, 150));
@@ -82,7 +42,6 @@ Random rnd =new Random();
         testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 91, 3));
         testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 152, 39));
         testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 35, 69));
-
         testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 12, 12));
         testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 23, 123));
         testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 405, 410));
@@ -90,38 +49,10 @@ Random rnd =new Random();
         testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 125, 89));
         testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 123, 501));
 
-        testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 350, 390));
-        testSelecteds.add(new Circle(9, Color.YELLOW, Color.YELLOW, x, y));
-
-        testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 350, 390));
-        testSelecteds.add(new Circle(9, Color.YELLOW, Color.YELLOW, x, y));
-
-        testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 350, 390));
-        testSelecteds.add(new Circle(9, Color.YELLOW, Color.YELLOW, x, y));
-
-        testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 350, 390));
-        testSelecteds.add(new Circle(9, Color.YELLOW, Color.YELLOW, x, y));
-
-        testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 350, 390));
-        testSelecteds.add(new Circle(9, Color.YELLOW, Color.YELLOW, x, y));
-
-        testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 350, 390));
-        testSelecteds.add(new Circle(9, Color.YELLOW, Color.YELLOW, x, y));
-
-        testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 350, 390));
-        testSelecteds.add(new Circle(9, Color.YELLOW, Color.YELLOW, x, y));
-
-        testCases.add(new Circle(9, Color.BLUE, Color.SILVER, 350, 390));
-        testSelecteds.add(new Circle(9, Color.YELLOW, Color.YELLOW, x, y));
-
-
-        // return new ObservableList<Shape>((Shape[]) testCases.toArray(), (Shape[]) testSelecteds.toArray());
-
         lists.add(testCases);
         lists.add(testSelecteds);
         return lists;
         }
-      //  return (ObservableList<Shape>) new ListPair<>(testCases, testSelecteds);
 
     public static List <Shape> solve(List<Shape> tests,double x, double y) {
         // Реализация эталонного решения
@@ -157,8 +88,7 @@ static boolean check=false;
                     // Сравнение результатов с ожидаемыми значениями
                     if (point.getColorBorder().equals(testSelected.getColorBorder()) && point.getColorFill().equals(testSelected.getColorFill())
                             && point.contains(testSelected.getX(), testSelected.getY())) {
-//                   if(!list.containsKey(test)&& !list.containsValue(point)&&!list.containsKey(point)&& !list.containsValue(test)) {
-//                       list.put(test,point);
+
 
                         System.out.println("Test PASSED: " + point.descriptor() + "\t" + testSelected.descriptor());
                         break;
